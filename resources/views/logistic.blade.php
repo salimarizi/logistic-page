b819058e842da039bead028b705217ea8f2e7616
salimarizi <salim9asmp1pdl@gmail.com>
2023-03-10 13:00:32 +0700

    setup project and environment

---
+++ resources/views/logistic.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Add Bootstrap 4 to Laravel 8</title>
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" />
+
+</head>
+
+<body>
+    <main role="main" class="container">
+        <div id="app">
+            <logistic-page></logistic-page>
+        </div>
+    </main><!-- /.container -->
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</body>
+
+</html>
